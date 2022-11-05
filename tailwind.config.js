106a48b5def2893b1d6a670e5c380d84f16ab809
Joey Yax <joey@joeyyax.com>
2022-11-05 13:31:35 -0700

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require("tailwindcss/colors")
+
+let breakpoints = Object.assign({}, defaultTheme.screens)
+delete breakpoints["2xl"] // delete the 2xl breakpoint
+
+module.exports = {
+  content: ["./{components,pages,styles}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        screens: breakpoints,
+        center: true,
+      },
+      colors: {
+        action: {
+          default: colors.pink["600"],
+          base: colors.pink["600"],
+          hover: colors.pink["500"],
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
